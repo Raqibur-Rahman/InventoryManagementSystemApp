90f451998a130eede56b0fdf54fb52441af40c96
Raqibur-Rahman <raqibur.edu@gmail.com>
2023-03-09 15:04:14 +0600

    Add project files.

---
+++ UserInformationAPI/Controllers/ContactsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserInformationAPI.Data;
+
+namespace UserInformationAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactsController : Controller
+    {
+        private readonly ContactsAPIDbContext dbContext;
+
+        public ContactsController(ContactsAPIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetContacts()
+        {
+
+            return Ok(dbContext.Contacts.ToList());
+
+           
+        }
+    }
+}
